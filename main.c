5e48b441dc7d3aeb2bbd92c18bc7c25797f214b9
Joel Moyal <j.moyal@student.fontys.nl>
2024-04-22 17:21:04 -0600

    adding Guessing Game

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+int SecretNumber = 12;
+int guess;
+int guessCount = 0;
+int guessLimit = 3;
+int outOfGuesses = 0; 
+
+printf ("Guess a number between 0 and 15. \nwith a limit of 3 tries! ");
+printf ("Enter a number: ");
+scanf ("%d", &guess);
+
+while (guess != SecretNumber && outOfGuesses == 0){
+    if (guessCount < guessLimit){
+    printf ("Enter a number: ");
+    scanf ("%d", &guess);
+    guessCount++;
+    }
+    else {
+        outOfGuesses = 1;
+    }
+}
+
+if (outOfGuesses == 1){
+    printf ("You Lose!");
+    return 0;
+}
+
+printf ("congrats, You Win!");
+        return 0;
+}
